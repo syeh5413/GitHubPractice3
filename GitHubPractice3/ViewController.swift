636bdc2054c95c5e626ca187acb80ee09c03c230
Sam Yeh <syeh5413@stu.d214.org>
2022-11-21 11:13:17 -0600

    created inaction and alert

--- GitHubPractice3/ViewController.swift
+++ GitHubPractice3/ViewController.swift
@@ -8,23 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-
     
-     @IBOutlet weak var tableView: UITableView!
-     @IBOutlet weak var textField: UITextField!
-     var array = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    var array = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    tableView.dataSource = self
-}
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return array.count
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    <#code#>
-}
-
+        tableView.dataSource = self
+    }
+    
+    @IBAction func whenButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Pull Item from TextField?", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        let yesButton = UIAlertAction(title: "Yes", style: .default, handler: { alert in
+            textFieldHandler()
+        })
+        
+        alert.addAction(yesButton)
+        present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
 
