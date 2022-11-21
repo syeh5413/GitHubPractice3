6a915e2a8f9eb7180a510df7d7e95de61569ce45
Sam Yeh <syeh5413@stu.d214.org>
2022-11-21 11:15:09 -0600

    added back my code

--- GitHubPractice3/ViewController.swift
+++ GitHubPractice3/ViewController.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-
+    
     let word = ""
-     @IBOutlet weak var tableView: UITableView!
-     @IBOutlet weak var textField: UITextField!
-     var array = [String]()
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var textField: UITextField!
+    var array = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    tableView.dataSource = self
-}
+        tableView.dataSource = self
+    }
+    
+    @IBAction func whenButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Pull Item from TextField?", message: nil, preferredStyle: UIAlertController.Style.alert)
+        
+        let yesButton = UIAlertAction(title: "Yes", style: .default, handler: { alert in
+            textFieldHandler()
+        })
+        
+        alert.addAction(yesButton)
+        present(alert, animated: true)
+    }
+    
     func textFieldHandler() -> UIAlertAction {
         textField.text = word
         array.append(word)
@@ -25,17 +37,17 @@ class ViewController: UIViewController, UITableViewDataSource {
         textField.text = ""
     }
     
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return array.count
-}
-
-func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
-    var content = cell.defaultContentConfiguration()
-   content.text = array[indexPath.row]
-    cell.contentConfiguration = content
-    return cell
-}
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = array[indexPath.row]
+        cell.contentConfiguration = content
+        return cell
+    }
+    
 }
 
